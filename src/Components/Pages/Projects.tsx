97d48060d78c7fc338e220c3e35ee0a34bb77325
Cameron1044 <cameronpelkey.808@gmail.com>
2023-08-19 15:33:43 -0600

    added projects modal

--- src/Components/Pages/Projects.tsx
+++ src/Components/Pages/Projects.tsx
@@ -1,84 +1,112 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styles from '../../Styles/Projects.module.scss';
-import IMG1 from '../../Images/Project1.png';
+import projectData from '../Data/projects.json';
 
-// Define your project data and its type
+interface Media {
+  type: 'image' | 'video';
+  src: string;
+}
+
 interface Project {
   id: number;
   title: string;
-  image: string;
+  media: Media[];
   description: string;
 }
 
-const projects: Project[] = [
-  {
-    id: 1,
-    title: 'Project 1',
-    image: IMG1,
-    description: 'This is project 1.'
-  },
-  {
-    id: 2,
-    title: 'Project 2',
-    image: IMG1,
-    description: 'This is project 2.'
-  },
-  {
-    id: 3,
-    title: 'Project 2',
-    image: IMG1,
-    description: 'This is project 2.'
-  },
-  {
-    id: 4,
-    title: 'Project 2',
-    image: IMG1,
-    description: 'This is project 2.'
-  },
-  {
-    id: 5,
-    title: 'Project 2',
-    image: IMG1,
-    description: 'This is project 2.'
-  },
-  {
-    id: 6,
-    title: 'Project 2',
-    image: IMG1,
-    description: 'This is project 2.'
-  },
-  // more projects here...
-];
-
 function Projects() {
+  const [closing, setClosing] = useState(false);
+  const [projects, setProjects] = useState<Project[]>([]);
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+
+  useEffect(() => {
+    setProjects(projectData as Project[]);
+  }, []);
+
+  function nextImage() {
+    console.log("Next image triggered");
+    if (selectedProject && currentImageIndex < selectedProject.media.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+    console.log(currentImageIndex)
+  }
+  
+  function prevImage() {
+    console.log("Prev image triggered");
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+    console.log(currentImageIndex)
+  }
+
+  function startCloseProject() {
+    setClosing(true);
+  }
 
   function openProject(project: Project) {
     setSelectedProject(project);
   }
 
-  function closeProject() {
-    setSelectedProject(null);
-  }
+  const closeProject = useCallback(() => {
+    if (closing) {
+      setSelectedProject(null);
+      setCurrentImageIndex(0);
+      setClosing(false);
+    }
+  }, [closing]);
+
+  useEffect(() => {
+    function handleEscape(event: KeyboardEvent) {
+      if (event.key === 'Escape' && selectedProject) {
+        closeProject();
+      }
+    }
+  
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, [selectedProject, closeProject]);
 
   return (
     <div className={styles.projects}>
       {projects.map(project => (
-        <div key={project.id} className={styles.projectCard} onClick={() => openProject(project)}>
-          <img src={project.image} alt={project.title} />
+        <div key={project.id} className={styles.projectCard} onClick={() => openProject(project)} role="button" tabIndex={0}>
           <h2>{project.title}</h2>
+          <div className={styles.imgContainer}>
+            {project.media[0].type === 'image' ? (
+              <img src={project.media[0].src} alt={`Slide ${currentImageIndex + 1}`} />
+            ) : (
+              <video src={project.media[0].src} autoPlay loop muted playsInline />
+            )}
+          </div>
         </div>
       ))}
       {selectedProject && (
-        <div className={styles.projectModal}>
-          <button onClick={closeProject}>Close</button>
-          <h2>{selectedProject.title}</h2>
-          <p>{selectedProject.description}</p>
-        </div>
+        <>
+          <div className={`${styles.overlay} ${closing ? styles.FadeOut : ''}`} onClick={startCloseProject}></div>
+          <div className={`${styles.projectModal} ${closing ? styles.bubbleOut : ''}`} onAnimationEnd={closeProject}>
+            <button onClick={startCloseProject}>Close</button>
+            <h2>{selectedProject.title}</h2>
+            <div className={styles.contentContainer}>
+              <div className={styles.gallery}>
+                {selectedProject.media[currentImageIndex].type === 'image' ? (
+                  <img src={selectedProject.media[currentImageIndex].src} alt={`Slide ${currentImageIndex + 1}`}/>
+                ) : (
+                  <video src={selectedProject.media[currentImageIndex].src} autoPlay loop muted playsInline/>
+                )}
+                <button onClick={prevImage} disabled={currentImageIndex === 0}>Previous</button>
+                <button onClick={nextImage} disabled={currentImageIndex === selectedProject.media.length - 1}>Next</button>
+              </div>
+              <p>{selectedProject.description}</p>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
 }
 
 export default Projects;
-
