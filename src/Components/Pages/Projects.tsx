c95db136306ad89ba6dcbf2bffebb9af241bf7f0
Cameron1044 <cameronpelkey.808@gmail.com>
2023-08-20 14:24:36 -0600

    new

--- src/Components/Pages/Projects.tsx
+++ src/Components/Pages/Projects.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import styles from '../../Styles/Projects.module.scss';
 import projectData from '../Data/projects.json';
+import classNames from 'classnames';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 interface Media {
   type: 'image' | 'video';
@@ -74,34 +78,57 @@ function Projects() {
     <body>
       <div className={styles.projects}>
         {projects.map(project => (
-          <div key={project.id} className={styles.projectCard} onClick={() => openProject(project)} role="button" tabIndex={0}>
-            <h2>{project.title}</h2>
-            <div className={styles.imgContainer}>
+          <div className={styles.projects_card} key={project.id} onClick={() => openProject(project)} role="button" tabIndex={0}>
+            <h2 className={styles.projects_card_h2}>{project.title}</h2>
+            <div className={styles.projects_card_image_container}>
               {project.media[0].type === 'image' ? (
-                <img src={project.media[0].src} alt={`Slide ${currentImageIndex + 1}`} />
+                <img className={styles.projects_card_media} src={project.media[0].src} alt={`Slide ${currentImageIndex + 1}`} />
               ) : (
-                <video src={project.media[0].src} autoPlay loop muted playsInline />
+                <video className={styles.projects_card_media} src={project.media[0].src} autoPlay loop muted playsInline />
               )}
             </div>
           </div>
         ))}
         {selectedProject && (
           <>
-            <div className={`${styles.overlay} ${closing ? styles.FadeOut : ''}`} onClick={startCloseProject}></div>
-            <div className={`${styles.projectModal} ${closing ? styles.bubbleOut : ''}`} onAnimationEnd={closeProject}>
-              <button onClick={startCloseProject}>Close</button>
-              <h2>{selectedProject.title}</h2>
-              <div className={styles.contentContainer}>
-                <div className={styles.gallery}>
-                  {selectedProject.media[currentImageIndex].type === 'image' ? (
-                    <img src={selectedProject.media[currentImageIndex].src} alt={`Slide ${currentImageIndex + 1}`}/>
-                  ) : (
-                    <video src={selectedProject.media[currentImageIndex].src} autoPlay loop muted playsInline/>
-                  )}
-                  <button onClick={prevImage} disabled={currentImageIndex === 0}>Previous</button>
-                  <button onClick={nextImage} disabled={currentImageIndex === selectedProject.media.length - 1}>Next</button>
+            <div className={`${styles.projects_modal_overlay} ${closing ? styles.FadeOut : ''}`} onClick={startCloseProject}></div>
+            <div className={`${styles.projects_modal} ${closing ? styles.bubbleOut : ''}`} onAnimationEnd={closeProject}>
+              <div className={styles.projects_modal_header}>
+                <h2 className={styles.projects_modal_header_h2} >{selectedProject.title}</h2>
+                <button className={styles.projects_modal_header_button} onClick={startCloseProject}><FontAwesomeIcon icon={faXmark} className={styles.xIcon}/></button>
+              </div>
+              <div className={styles.projects_modal_content}>
+                <div className={styles.projects_modal_gallery}>
+                  <div className={styles.projects_modal_gallery_media_container}>
+                    {selectedProject.media[currentImageIndex].type === 'image' ? (
+                      <img className={styles.projects_modal_gallery_media} src={selectedProject.media[currentImageIndex].src} alt={`Slide ${currentImageIndex + 1}`}/>
+                    ) : (
+                      <video className={styles.projects_modal_gallery_media} src={selectedProject.media[currentImageIndex].src} autoPlay loop muted playsInline/>
+                    )}
+                  
+                  <div className={styles.projects_modal_gallery_button_container}>
+                    <button className={classNames(styles.projects_modal_gallery_button, styles.prev)} onClick={prevImage}>
+                      <FaChevronLeft className={styles.projects_modal_gallery_button_icon}/>
+                    </button>
+                    {selectedProject.media.map((_, index) => (
+                      <div key={index} className={`${styles.projects_modal_gallery_button_pagination} ${currentImageIndex === index ? styles.active : ''}`}/>
+                    ))}
+                    <button className={classNames(styles.projects_modal_gallery_button, styles.next)} onClick={nextImage}>
+                      <FaChevronRight className={styles.projects_modal_gallery_button_icon}/>
+                    </button>
+                    </div>
+                  </div>
                 </div>
-                <p>{selectedProject.description}</p>
+                <div className={styles.projects_modal_content_text_container}>
+                  <div className={classNames(styles.projects_modal_content_subsection, styles.top)}>
+                    <h1 className={styles.projects_modal_content_h1}>Objective</h1>
+                    <p className={styles.projects_modal_content_p}>{selectedProject.description}</p>
+                  </div>
+                  <div className={styles.projects_modal_content_subsection}>
+                    <h1 className={styles.projects_modal_content_h1}>Results</h1>
+                    <p className={styles.projects_modal_content_p}>{selectedProject.description}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </>
